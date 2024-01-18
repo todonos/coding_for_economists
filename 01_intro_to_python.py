ac3ce02aaa0fd37b593d72c8f5cfc297231c5e20
todonos <stodgerel@gmail.com>
2024-01-18 13:21:30 +0000

    Added python file

---
+++ 01_intro_to_python.py
@@ -0,0 +1,32 @@
+#1. Navigate back to raw/ (<cd ..>)
+#2. Create a folder called raw/country-codes (<mkdir>)
+#3. Download the country-codes.csv file into country_codes/ (curl -L -o fname url)
+
+url = 'https://github.com/codedthinking/tender-home-bias/releases/download/v1.0/ted-sample.csv'
+
+#2024.01.16
+#Exercise: Indexing and slicing
+
+#Use indexing, slicing to fill in the following output
+#copy/ paste is not allowed
+
+file_name = url[-14:]
+print(file_name)  #ted-sample.csv
+
+protocol = url[0:5]
+print(protocol)  #https
+
+host_name = url[8:18]
+print(host_name)  #'github.com'
+
+#Use string composition to construct http://github.com/ted-sample.csv
+all = protocol + '://' + host_name + '/codedthinking/tender-home-bias/releases/download/v1.0/' + file_name
+print(all)
+
+
+18 Jan 2024
+Lecture 4
+# Project: Home Bias Tender Data
+## Getting to know the Shell, git and Makefile
+
+In this project we download data form the Shell using curl and then create a derivative data file using csvkit
