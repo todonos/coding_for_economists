9ad5c7c7741934095b33055428be9ae4523e715c
todonos <stodgerel@gmail.com>
2024-01-18 13:56:23 +0000

    Added script for dictionaries

---
+++ 04_python_dictionaries.py
@@ -0,0 +1,14 @@
+# First lookk at dictionaries
+
+# A dictionary is a collection of key-value pairs
+
+#Syntax:
+myDict = {"Dog": "Barks", "Cat": "Meows", "Cow": "Moo"}
+
+print(myDict)
+
+# We cannot use the same indexing syntax as with lists/ str
+# print(myDict[0]) #error
+
+# We can use the key to get the value
+print(myDict["Dog"])
